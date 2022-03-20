b80205e063c1d2b396d8f561de320962ca6c973c
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2022-03-20 15:54:55 +0530

    tasks

--- client/src/components/_dashboard/app/AppTasks.js
+++ client/src/components/_dashboard/app/AppTasks.js
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import TextField from '@mui/material/TextField';
+import {useState} from "react";
 import { Form, FormikProvider, useFormik } from 'formik';
 // material
 import {
@@ -14,14 +16,14 @@ import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 import Tooltip from '@mui/material/Tooltip';
 
-const TASKS = [
-  'Create FireStone Logo',
+let TASKS = [
+ 
   'Write a Blog if required',
-  'Check the Pending Dues',
-  'Scoping & Estimations',
-  'Check Social Media Followers'
+
+
 ];
 
+TASKS = JSON.parse(localStorage.getItem("task")) === null? TASKS :  JSON.parse(localStorage.getItem("task"));
 TaskItem.propTypes = {
   task: PropTypes.string,
   checked: PropTypes.bool,
@@ -31,12 +33,15 @@ TaskItem.propTypes = {
 function TaskItem({ task, checked, formik, ...other }) {
   const { getFieldProps } = formik;
 
+
   return (
     <Stack direction="row" justifyContent="space-between" sx={{ py: 0.75 }}>
       <FormControlLabel
         control={
           <Checkbox {...getFieldProps('checked')} value={task} checked={checked} {...other} />
+         
         }
+        
         label={
           <Typography
             variant="body2"
@@ -56,16 +61,46 @@ function TaskItem({ task, checked, formik, ...other }) {
 }
 
 export default function AppTasks() {
+  const [rendersite , setrendersite] = useState(false);
+  let [items, setItems] = useState("");
+
+  const Add = () => {
+      items = items.trim();
+      if(items.length === 0){
+        alert("Add an Proper Task");
+        return;
+      }
+     
+   
+      TASKS.push(items);
+       setItems("");
+       localStorage.setItem("task", JSON.stringify(TASKS));
+     
+     if(!rendersite) {
+      setrendersite(true);
+     }else{
+      setrendersite(false);
+     }
+   }
   const formik = useFormik({
     initialValues: {
-      checked: [TASKS[4]]
+      checked: [TASKS[1]]
     },
     onSubmit: (values) => {
+      // console.log("line 86")
       console.log(values);
     }
   });
+  
+  function insertTask(e) {
+    setItems(e.target.value);
+  }
 
   const { values, handleSubmit } = formik;
+  // let result = values.checked;
+  // console.log(result);
+  TASKS = TASKS.filter((e1)=> !values.checked.includes(e1))
+  localStorage.setItem("task", JSON.stringify(TASKS));
 
   return (
     <Card>
@@ -73,7 +108,10 @@ export default function AppTasks() {
       <Box sx={{ px: 3, py: 1 }}>
         <FormikProvider value={formik}>
           <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
-            {TASKS.map((task) => (
+            
+            
+            {TASKS.map((task) => ( 
+              
               <TaskItem
                 key={task}
                 task={task}
@@ -83,8 +121,12 @@ export default function AppTasks() {
             ))}
           </Form>
         </FormikProvider>
+        <TextField id="outlined-basic" label= "ADD TASK" variant="outlined" value = {items} onChange = {insertTask}/ >
+        
+        
+
         <Tooltip title="Add Task" >
-        <IconButton aria-label="delete" size="medium" style ={{backgroundColor : "green", color:"white"}}>
+        <IconButton aria-label="delete" size="medium" style ={{backgroundColor : "green", color:"white", marginLeft:"10px", marginTop: "5px"}} onClick = {Add}>
               <AddIcon  fontSize="inherit" />
         </IconButton>
         </Tooltip>
