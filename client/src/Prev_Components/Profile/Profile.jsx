bda2838baade14a5eb3333c688285d28407828ba
Deepak Yadav <deepaky.tt.19@nitj.ac.in>
2022-03-20 16:48:45 +0530

    Updating Profile pages

--- client/src/Prev_Components/Profile/Profile.jsx
+++ client/src/Prev_Components/Profile/Profile.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./Profile.css"
 import "./Profiles.js" 
 import {TextField } from '@mui/material';
 import deepak from "../../images/deepak2.jpeg"
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
+  const navigate = useNavigate();
+    const[user , setuser] = useState({
+      name: JSON.parse(localStorage.getItem('name')),
+      email:  JSON.parse(localStorage.getItem('user_email'))
+    })
+
+    const sendData = async(e) =>{
+      const {name, email} = user ;
+      e.preventDefault() ;
+      const response =  await fetch(`http://localhost:80/admin/SaveChangesToProfile`,{
+        method : "POST",
+        headers :{
+            "Accept":"application/json",
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({
+            email,name 
+        })
+    });
+    const awaited_response = await response.json();
+    if(awaited_response.status === 200){
+      alert("Profile Updated!");
+      localStorage.setItem("name" , JSON.stringify(user.name));
+      localStorage.setItem("email" , JSON.stringify(user.email));
+      
+    }
+    else{
+      alert("An Error Occured!");
+
+    }
+
+
+    }
+
+    const changeData = (e) =>{
+      let input_name = e.target.name ;
+     let  value = e.target.value ;
+    setuser({...user , [input_name]:value})
+
+    }
+    const redirect = () =>{
+        navigate("/admin/app" , {replace:true})
+    }
+
   return (
       <>
     <div className="wrapper">
@@ -15,7 +60,7 @@ const Profile = () => {
 
     <div className="profile-card__cnt js-profile-cnt">
       <div className="profile-card__name">
-      <TextField   label = "Your Name" value = "Deepak Yadav"/>
+      <TextField   label = "Your Name" name = 'name' value = {user.name} onChange = {changeData} />
       </div>
       <div className="profile-card__txt">Software<strong>   Developer</strong></div>
       <div className="profile-card-loc">
@@ -28,15 +73,15 @@ const Profile = () => {
 
       <div className="profile-card-inf">
         <div className="profile-card-inf__item">
-           <TextField   label = "Your Email" value = "deepakrao0223@gmail.com" />
+           <TextField   label = "Your Email" name='email' value = {user.email} onChange = {changeData} />
         </div>
       </div>
 
      
 
       <div className="profile-card-ctr">
-        <button className="profile-card__button button--blue js-message-btn">Save</button>
-        <button className="profile-card__button button--orange">Don't Save</button>
+        <button className="profile-card__button button--blue js-message-btn" onClick = {sendData}>Save</button>
+        <button className="profile-card__button button--orange" onClick={redirect}>Don't Save</button>
       </div>
     </div>
 
